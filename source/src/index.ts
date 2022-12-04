8626262eddf71cede89ff632addd5042853c75ef
samurai-sa <69831034+samurai-sa@users.noreply.github.com>
2022-12-04 17:46:35 +0900

    wip

--- source/src/index.ts
+++ source/src/index.ts
@@ -1,3 +1,52 @@
+// ====
+// 4 章
+// ====
+
+// 関数の呼び出し
+function add(a: number, b: number) {
+  const sum = a + b;
+  console.log(sum);
+  return sum;
+}
+
+add.apply(null, [10, 20]); // => 30
+add.call(null, 10, 30); // => 30
+add.bind(null, 10, 40)(); // => 30
+
+function * createFibonacciGenerator() {
+  let a = 0;
+  let b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+
+// ジェネレーター
+let fibonacciGenerator = createFibonacciGenerator();
+fibonacciGenerator.next();
+fibonacciGenerator.next();
+fibonacciGenerator.next();
+fibonacciGenerator.next();
+fibonacciGenerator.next();
+fibonacciGenerator.next();
+
+// シグネチャ
+type Log = (message: string, userId?: string) => void
+
+let log: Log = (message, userId = 'Not signed in') => {
+  let time = new Date().toISOString();
+  console.log(time, message, userId)
+}
+
+log("こんにちは");
+
+// オーバーロード
+
+// ====
+// 3 章
+// ====
+
 let a: {
   b: number,
   c?: string,
