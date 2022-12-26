e7e11e56937ff20662d6f705a3d36a0454ac3d3a
satoshi-nakajima-uhuru <nakajima.satoshi@uhuru.jp>
2022-12-26 17:43:22 +0900

    add

--- source/src/index.ts
+++ source/src/index.ts
@@ -1,4 +1,62 @@
 // ====
+// 5 章
+// ====
+let set = new Set;
+set.add(1).add(2).add(3);
+set.has(2);
+set.has(4);
+
+type Color = 'Black' | 'White';
+type File = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 8 ;
+
+class Position {
+  constructor (
+    private file: File,
+    private rank: Rank
+  ){}
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+    }
+  }
+}
+
+abstract class Piece {
+  protected position: Position
+  constructor (
+    private readonly color: Color,
+    file: File,
+    rank: Rank
+  ){
+    this.position = new Position(file, rank)
+  }
+  moveTo(position: Position) {
+    this.position = this.position;
+  }
+  abstract canMoveTo(position: Position): boolean
+}
+
+
+class Game {
+  private pieces = Game.makePieces()
+  private static makePieces() {
+    return [
+      new King('White', 'E', 1),
+      new King('Black', 'E', 8),
+    ]
+  }
+}
+
+class King extends Piece {
+  canMoveTo(position: Position): boolean {
+    let distance = this.position.distanceFrom(position)
+    return distance.rank < 2 && distance.file < 2
+  }
+}
+
+// ====
 // 4 章
 // ====
 type Reduce<T> = {
